8f895a3ac56c712f348d9e92192869fe71ec8e3c
Barry Napier <barry.a.napier@gmail.com>
2025-04-02 08:21:11 +0000

    Enhance ESLint configuration with Prettier integration for improved code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js, prettier: prettierPlugin },
+    extends: ["js/recommended", "plugin:prettier/recommended"],
+  },
   tseslint.configs.recommended,
+  prettier,
 ]);
